0655c247c412af0c3ad072b418423c4f175f0132
Iustin Hilca <ihi@insero.com>
2017-09-26 13:24:05 +0200

    contact form

--- src/components/Header/Compo/Contact.vue
+++ src/components/Header/Compo/Contact.vue
@@ -1,16 +1,80 @@
 <template>
   <div class="container">
-    <div class="row">
-      <p> EEEEE </p>
-    </div>
+    <form>
+      <div class="row">
+        <div class="col-xs-12 cl-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Contact an instructor</h1>
+          <hr>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              class="form-control"
+              placeholder="Enter your name">
+          </div>
+
+          <div class="form-group">
+            <label for="mail">Mail</label>
+            <input
+              type="text"
+              id="mail"
+              class="form-control"
+              placeholder="Enter your mail">
+          </div>
+
+          <div class="form-group">
+            <label for="number">Number</label>
+            <input
+              type="text"
+              id="mail"
+              class="form-control"
+              placeholder="Enter your number">
+          </div>
+
+          <div class="form-group">
+            <label for="message">Message</label>
+            <br>
+            <textarea
+              id="message"
+              rows="5"
+              cols="100"
+              class="from-control"
+              placeholder="Enter your message"
+              v-model= "message"
+            ></textarea>
+          </div>
+        <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
+        </div>
+
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      userData: {
+        username: ' ',
+        mail: '',
+        age: 0
+      },
+      message: '',
+      isSubmitted: false
+    };
+  },
+  methods: {
+    submitted () {
+      this.isSubmitted = true;
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
+textarea {
+  resize: none;
+}
 </style>
