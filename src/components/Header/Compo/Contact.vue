f539a17c1cf7f222bfd168e69ef3aa7bc4de4517
Iustin Hilca <ihi@insero.com>
2017-09-26 15:52:49 +0200

    before leave work

--- src/components/Header/Compo/Contact.vue
+++ src/components/Header/Compo/Contact.vue
@@ -31,23 +31,28 @@
               class="form-control"
               placeholder="Enter your number">
           </div>
-
-          <div class="form-group">
-            <label for="message">Message</label>
-            <br>
-            <textarea
-              id="message"
-              rows="5"
-              cols="100"
-              class="from-control"
-              placeholder="Enter your message"
-              v-model= "message"
-            ></textarea>
-          </div>
-        <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
         </div>
-
       </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+              <label for="message">Message</label><br>
+              <textarea
+                id="message"
+                rows="5"
+                class="from-control"
+                placeholder="Enter your message"
+                v-model= "message"
+              ></textarea>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+              <button
+                class="btn btn-primary"
+                @click.prevent="submitted">Submit!
+              </button>
+          </div>
+        </div>
     </form>
   </div>
 </template>
